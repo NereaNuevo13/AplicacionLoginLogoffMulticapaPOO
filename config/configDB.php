7873e568dcd7bf73622c91301065cdd79108d691
NereaNuevo13 <nerekiru@gmail.com>
2021-01-15 08:41:22 +0100

    Aplicación Log In Log Out

--- config/configDB.php
+++ config/configDB.php
@@ -1,23 +1,14 @@
 <?php
-    //CONFIGURACIÓN CASA
-    /*
-    define('DNS', 'mysql:host=192.168.1.215;dbname=DAW215DBProyectoTema5');//Dirección IP y nombre de la base de datos
-    define('USER', 'usuarioDAW215DBProyectoTema5');//Nombre de usuario de la base de datos
-    define('PASSWORD', 'P@ssw0rd');//Contraseña del usuario de la base de datos
-    */
 
+//ENTORNO DESARROLLO
+define("DNS", "mysql:host=192.168.20.19;dbname=DAW214DBProyectoTema5");
+define("USER", "usuarioDAW214DBProyectoTema5");
+define("PASSWORD", "paso");
 
-    //CONFIGURACIÓN ENTORNO DE DESARROLLO
-    
-    define('DNS', 'mysql:host=192.168.20.19;dbname=DAW215DBProyectoTema5;charset=utf8');//Dirección IP y nombre de la base de datos
-    define('USER', 'usuarioDAW215DBProyectoTema5');//Nombre de usuario de la base de datos
-    define('PASSWORD', 'P@ssw0rd');//Contraseña del usuario de la base de datos
-   
-
-    //CONFIGURACIÓN 1&1
-   /*
-    define('DNS', 'mysql:host=db5000278682.hosting-data.io;dbname=dbs272026');//Dirección IP y nombre de la base de datos
-    define('USER', 'dbu287777');//Nombre de usuario de la base de datos
-    define('PASSWORD', 'Covid1234$');//Contraseña del usuario de la base de datos
-    */
+//1 AND 1
+/*
+  define ("HOST", "mysql:host=db5001094498.hosting-data.io;dbname=dbs939516");
+  define ("USUARIO", "dbu1114024");
+  define ("PASS", "Covid1234$");
+ */
 ?>
